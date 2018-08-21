5c0302af246a45ce523212192687e5edac885823
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-08-21 23:27:09 +0200

    Initial import

---
+++ firmware/hw_test_speedometer.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=hw_test_speedometer.X.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/hw_test_speedometer.X.production.hex
+CND_PACKAGE_DIR_Debug=${CND_DISTDIR}/Debug/package
+CND_PACKAGE_NAME_Debug=hwtestspeedometer.x.tar
+CND_PACKAGE_PATH_Debug=${CND_DISTDIR}/Debug/package/hwtestspeedometer.x.tar
